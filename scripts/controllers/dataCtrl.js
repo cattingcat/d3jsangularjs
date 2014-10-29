23644406e5d55dc7d2c0a161d52dbaa7ef44c925
Pavel_Mihailov <Pavel_Mihailov@epam.com>
2014-10-29 18:42:17 +0400

    + add more test data, add some config values for pack layout

--- scripts/controllers/dataCtrl.js
+++ scripts/controllers/dataCtrl.js
@@ -3,47 +3,137 @@ var dataCtrl = (function(){
 	var factory = function($scope, $log){
 		$scope.data = {
 			"quadrant" : "Cache",
-			"items" : [
+			"items": [
+                {
+                    "name": "Velocity",
+                    "movement": "Stable",
+                    "maturity": "Use",
+                    "source": {
+                        "practical": 5,
+                        "theoretical": 4
+                    },
+                    id: 1
+                },
 				{
-					"name" : "Velocity",
-					"movement" : "Stable",
-					"maturity" : "Use",
-					"source" : {
-						"practical" : 5,
-						"theoretical" : 4
-					},
-					id : 1
+				    "name": "Redis",
+				    "movement": "Down",
+				    "maturity": "Use",
+				    "source": {
+				        "practical": 0,
+				        "theoretical": 4
+				    },
+				    id: 6
 				},
 				{
-					"name" : "Redis",
-					"movement" : "Down",
-					"maturity" : "Use",
-					"source" : {
-						"practical" : 0,
-						"theoretical" : 4
-					},
-					id : 6
+				    "name": "Azure Cache Service",
+				    "movement": "Up",
+				    "maturity": "Use with care",
+				    "source": {
+				        "practical": 5,
+				        "theoretical": 3
+				    },
+				    id: 7
 				},
 				{
-					"name" : "Azure Cache Service",
-					"movement" : "Up",
-					"maturity" : "Use with care",
-					"source" : {
-						"practical" : 5,
-						"theoretical" : 3
-					},
-					id : 7
-				},
-				{
-					"name" : "Memcached",
-					"movement" : "Stable",
-					"maturity" : "Be informed",
-					"source" : {
-						"practical" : 1,
-						"theoretical" : 5
-					}, 
-					id : 9
+				    "name": "Memcached",
+				    "movement": "Stable",
+				    "maturity": "Be informed",
+				    "source": {
+				        "practical": 1,
+				        "theoretical": 5
+				    },
+				    id: 9
 				}
+				//{
+				//	"name" : "Velocity",
+				//	"movement" : "Stable",
+				//	"maturity" : "Use",
+				//	"source" : {
+				//		"practical" : 1,
+				//		"theoretical" :1
+				//	},
+				//	id : 1
+				//},
+				//{
+				//	"name" : "Redis",
+				//	"movement" : "Down",
+				//	"maturity" : "Use",
+				//	"source" : {
+				//		"practical" : 4,
+				//		"theoretical" : 1
+				//	},
+				//	id : 2
+				//},
+				//{
+				//	"name" : "Azure Cache Service",
+				//	"movement" : "Up",
+				//	"maturity" : "Use",
+				//	"source" : {
+				//		"practical" : 5,
+				//		"theoretical" : 3
+				//	},
+				//	id : 3
+				//},
+				//{
+				//	"name" : "Memcached",
+				//	"movement" : "Stable",
+				//	"maturity" : "Use",
+				//	"source" : {
+				//		"practical" : 1,
+				//		"theoretical" : 5
+				//	}, 
+				//	id : 4
+				//},
+				//{
+				//    "name": "Microsoft Service",
+				//    "movement": "Stable",
+				//    "maturity": "Use",
+				//    "source": {
+				//        "practical": 19,
+				//        "theoretical": 3
+				//    },
+				//    id: 5
+				//},
+				//{
+				//    "name": "Google Chrom Service",
+				//    "movement": "Stable",
+				//    "maturity": "Use",
+				//    "source": {
+				//        "practical": 1,
+				//        "theoretical": 19
+				//    },
+				//    id: 6
+				//},
+				//{
+				//    "name": "Google Chrom Service 2",
+				//    "movement": "Stable",
+				//    "maturity": "Use with care",
+				//    "source": {
+				//        "practical": 1,
+				//        "theoretical": 10
+				//    },
+				//    id: 7
+				//},
+				//{
+				//    "name": "Google Chrom Service 3",
+				//    "movement": "Down",
+				//    "maturity": "Be informed",
+				//    "source": {
+				//        "practical": 1,
+				//        "theoretical": 5
+				//    },
+				//    id: 8
+				//},
+				//{
+				//    "name": "Google Chrom Service 4",
+				//    "movement": "Down",
+				//    "maturity": "Avoid",
+				//    "source": {
+				//        "practical": 1,
+				//        "theoretical": 4
+				//    },
+				//    id: 9
+				//}
 			]
 		};
 		$scope.filter = {
@@ -59,15 +149,15 @@ var dataCtrl = (function(){
 				var maturities = ['Use', 'Use with care', 'Be informed', 'Avoid'];
 
 				var trnd = Math.floor(Math.random() * 3);
-				var trends = ['Up', 'Stable', 'Down']
+			    var trends = ['Up', 'Stable', 'Down'];
 				
 				$scope.data.items.push({
 						"name" : "Java",
 						"movement" : trends[trnd],
 						"maturity" : maturities[mrnd],
 						"source" : {
-							"practical" : (Math.random() * 10),
-							"theoretical" : (Math.random() * 10)
+							"practical" : Math.round((Math.random() * 10)),
+							"theoretical" : Math.round((Math.random() * 10))
 						},
 						id : 1
 					});
--- scripts/directives/plainView.js
+++ scripts/directives/plainView.js
@@ -176,26 +176,29 @@ var plainView = (function(){
 									var currArea = areaNames[i];
 									var currAreaWidth = widthArray[i];
 									var currAreaItems = viewModel.filter(function(itm){ return itm.maturity === currArea });
-									
-									for(var j = 0; j < 4; ++j){
+
+								    var radius = 3.5;
+								    var areasQuantity = 4;
+                                    var offset = 100 / areasQuantity;
+
+                                    for (var j = 0; j < areasQuantity; ++j) {
 										var pack = d3.layout.pack()
-											.size([currAreaWidth - 5, 25])
-											.radius(3.5)
+											.size([currAreaWidth - 5, offset])
+											.radius(radius)
 											.value(function(d) { 
 												return d.practical + d.theoretical; 
-											});									
+											});
 
-										var tmpItems = currAreaItems.filter(function(itm){
-											var tmp = (itm.theoretical * 100.0 / (itm.theoretical + itm.practical))
-											return tmp <= (j + 1) * 25 && tmp > j * 25;
-										})
-										
+                                        var tmpItems = currAreaItems.filter(function(itm) {
+                                            var tmp = (itm.theoretical * 100.0 / (itm.theoretical + itm.practical));
+                                            return tmp <= (j + 1) * offset && tmp > j * offset;
+                                        });
 
 										pack.nodes({name: ' ', children: tmpItems});
 
 										tmpItems.map(function(itm){
 											itm.x += currAreaStart + 5; 
-											itm.y += j*25;
+											itm.y += j * offset;
 										});
 
 										res = res.concat(tmpItems);
