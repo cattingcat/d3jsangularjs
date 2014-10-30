7c4476b3da8158bb4e97d5a643e41b85dd407f00
mark_martynov@epam.com <Mark_Martynov@epam.com>
2014-10-30 13:56:17 +0400

    circle radar initial

--- scripts/controllers/dataCtrl.js
+++ scripts/controllers/dataCtrl.js
@@ -140,6 +140,121 @@ var dataCtrl = (function(){
 			trend: 'All',
 			recommendation: 'All'
 		};
+
+		$scope.circleData = [
+			{
+				"quadrant" : "Cache",
+				"items" : [
+					{
+						"name" : "Velocity(AppFabric Cache)",
+						"movement" : "Stable",
+						"maturity" : "Use",
+						"source" : {
+							"practical" : 0,
+							"theoretical" : 4
+						},
+					},
+					{
+						"name" : "Redis",
+						"movement" : "Stable",
+						"maturity" : "Use with care",
+						"source" : {
+							"practical" : 0,
+							"theoretical" : 4
+						},
+					},
+					{
+						"name" : "Azure Cache Service",
+						"movement" : "Up",
+						"maturity" : "Use with care",
+						"source" : {
+							"practical" : 0,
+							"theoretical" : 3
+						},
+					},
+					{
+						"name" : "Memcached",
+						"movement" : "Stable",
+						"maturity" : "Use",
+						"source" : {
+							"practical" : 1,
+							"theoretical" : 5
+						},
+					}
+				]
+			},
+			{
+				"quadrant":"Custom components",
+				"items":[
+					{
+						"name":"Native:Mobile",
+						"movement":"Up",
+						"maturity":"Use",
+						"source": {
+							"practical":4,
+							"theoretical":2
+						},
+					},
+
+					{
+						"name":"Native:Desktop",
+						"movement":"Down",
+						"maturity":"Use",
+						"source": {
+							"practical":7,
+							"theoretical":0
+						},
+					},
+
+					{
+						"name":"Web Services (no UI)",
+						"movement":"Stable",
+						"maturity":"Use",
+						"source": {
+							"practical":6,
+							"theoretical":0
+						},
+					},
+
+					{
+						"name":"System Service (no UI)",
+						"movement":"Stable",
+						"maturity":"Use",
+						"source": {
+							"practical":6,
+							"theoretical":0
+						},
+					},
+
+					{
+						"name":"Web:Mobile",
+						"movement":"Up",
+						"maturity":"Use",
+						"source": {
+							"practical":1,
+							"theoretical":5
+						},
+					},
+					{
+						"name":"Web:Desktop",
+						"movement":"Stable",
+						"maturity":"Use",
+						"source": {
+							"practical":7,
+							"theoretical":0
+						},
+					},
+					{
+						"name":"Wearable technologies",
+						"movement":"Up",
+						"maturity":"Be informed",
+						"source": {
+							"practical":0,
+							"theoretical":2
+						},
+					}
+				]
+			}];
 		
 		$scope.click = function(){
 			for (var i = 10 - 1; i >= 0; i--) {
--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-{
+[{
 	"quadrant" : "Cache",
 	"items" : [
 		{
@@ -9,7 +9,6 @@
 				"practical" : 0,
 				"theoretical" : 4
 			},
-			"itemId" : 16
 		},
 		{
 			"name" : "Redis",
@@ -19,7 +18,6 @@
 				"practical" : 0,
 				"theoretical" : 4
 			},
-			"itemId" : 17
 		},
 		{
 			"name" : "Azure Cache Service",
@@ -29,7 +27,6 @@
 				"practical" : 0,
 				"theoretical" : 3
 			},
-			"itemId" : 56
 		},
 		{
 			"name" : "Memcached",
@@ -39,7 +36,78 @@
 				"practical" : 1,
 				"theoretical" : 5
 			},
-			"itemId" : 15
 		}
 	]
-}
+},
+{
+	"quadrant":"Custom components",
+	"items":[
+		{
+			"name":"Native:Mobile",
+			"movement":"Up",
+			"maturity":"Use",
+			"source": {
+				"practical":4,
+				"theoretical":2
+			},
+		},
+
+		{
+			"name":"Native:Desktop",
+			"movement":"Down",
+			"maturity":"Use",
+			"source": {
+				"practical":7,
+				"theoretical":0
+			},
+		},
+
+		{
+			"name":"Web Services (no UI)",
+			"movement":"Stable",
+			"maturity":"Use",
+			"source": {
+				"practical":6,
+				"theoretical":0
+			},
+		},
+
+		{
+			"name":"System Service (no UI)",
+			"movement":"Stable",
+			"maturity":"Use",
+			"source": {
+				"practical":6,
+				"theoretical":0
+			},
+		},
+
+		{
+			"name":"Web:Mobile",
+			"movement":"Up",
+			"maturity":"Use",
+			"source": {
+				"practical":1,
+				"theoretical":5
+			},
+		},
+		{
+			"name":"Web:Desktop",
+			"movement":"Stable",
+			"maturity":"Use",
+			"source": {
+				"practical":7,
+				"theoretical":0
+			},
+		},
+		{
+			"name":"Wearable technologies",
+			"movement":"Up",
+			"maturity":"Be informed",
+			"source": {
+				"practical":0,
+				"theoretical":2
+			},
+		}
+	]
+}]
