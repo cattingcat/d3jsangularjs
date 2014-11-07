7a71446342ffbbb200544b3b8c8b908b4db82eec
mark_martynov@epam.com <Mark_Martynov@epam.com>
2014-11-07 18:22:08 +0400

    ng-view test

--- scripts/app.js
+++ scripts/app.js
@@ -4,12 +4,23 @@ define([
 	'directives/plainView'],
 	function(dataCtrl, circleView, plainView) {
 		'use strict';
-		var app = angular.module('app', []);
+		var app = angular.module('app', ['ngRoute']);
 
 		app
 			.controller('dataController', dataCtrl)
 			.directive('circleView', circleView)
-			.directive('plainView', plainView);
+			.directive('plainView', plainView)
+			.config(['$routeProvider', function($routeProvider){
+
+				$routeProvider.when('/hello', {
+					templateUrl: '/scripts/views/hello.tmpl.html'
+				});
+
+				$routeProvider.when('/radars', {
+					templateUrl: '/scripts/views/radars.tmpl.html'
+				});
+
+			}]);
 	}
 );
 
